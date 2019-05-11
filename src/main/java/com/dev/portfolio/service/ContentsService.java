ef9efd1053eeaaaec067037050f947db6e226518
jaeho214 <hyeonpig@gmail.com>
2019-05-11 16:56:56 +0900

    Serivce 클래스 생성

---
+++ src/main/java/com/dev/portfolio/service/ContentsService.java
@@ -0,0 +1,11 @@
+package com.dev.portfolio.service;
+/*
+자소서 항목들을 채우는 서비스
+자소서 삽입/ 자소서 수정/ 자소서 삭제/ 불러와서 출력
+ */
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ContentsService {
+}
