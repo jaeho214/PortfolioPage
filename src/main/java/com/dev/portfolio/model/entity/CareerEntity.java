59c97bf485b7b78daab87e5c1185e08a68e826a2
jaeho214 <hyeonpig@gmail.com>
2019-05-26 13:56:09 +0900

    career 삽입, 삭제, 갱신 테스트 완료

--- src/main/java/com/dev/portfolio/model/entity/CareerEntity.java
+++ src/main/java/com/dev/portfolio/model/entity/CareerEntity.java
@@ -4,12 +4,14 @@ import com.dev.portfolio.model.dto.CareerDto;
 import lombok.*;
 
 import javax.persistence.*;
+import java.lang.reflect.Member;
 
 /*
 경력사항 기입
  */
-@Entity @Setter @Getter
-@NoArgsConstructor @AllArgsConstructor
+@Entity
+@Setter @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "tbl_career")
 public class CareerEntity {
     @Id
@@ -26,5 +28,24 @@ public class CareerEntity {
     private String details; //업무 내용
     private String reason; // 퇴사사유
 
-    //public void updateCareer(CareerDto .)
+    @Builder
+    public CareerEntity(MemberEntity memberEntity, String company, String term, String department, String details, String reason){
+        this.member = memberEntity;
+        this.company = company;
+        this.term = term;
+        this.department = department;
+        this.details = details;
+        this.reason = reason;
+    }
+
+    public void updateCareer(CareerDto careerDto){
+        this.company = careerDto.getCompany();
+        this.department = careerDto.getDepartment();
+        this.term = careerDto.getTerm();
+        this.details = careerDto.getDetails();
+        this.reason = careerDto.getReason();
+    }
+
+
+
 }
