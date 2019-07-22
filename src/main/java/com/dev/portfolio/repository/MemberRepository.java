8c81857ee5e11a2f10e7c80ea5fbb1bd06be0427
jaeho214 <hyeonpig@gmail.com>
2019-07-22 23:22:55 +0900

    회원 관련 기능 구현 중

--- src/main/java/com/dev/portfolio/repository/MemberRepository.java
+++ src/main/java/com/dev/portfolio/repository/MemberRepository.java
@@ -1,14 +1,14 @@
 package com.dev.portfolio.repository;
 
 
-import com.dev.portfolio.model.dto.MemberDto;
-import com.dev.portfolio.model.entity.CareerEntity;
+
 import com.dev.portfolio.model.entity.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface MemberRepository extends JpaRepository<MemberEntity,Long> {
     Optional<MemberEntity> findById(String id);
 }
--- src/main/java/com/dev/portfolio/service/MemberService.java
+++ src/main/java/com/dev/portfolio/service/MemberService.java
@@ -7,15 +7,45 @@ package com.dev.portfolio.service;
 import com.dev.portfolio.exception.UserDefineException;
 import com.dev.portfolio.model.dto.MemberDto;
 import com.dev.portfolio.model.entity.MemberEntity;
+import com.dev.portfolio.model.entity.MemberRole;
 import com.dev.portfolio.repository.MemberRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-@Service
-public class MemberService {
-    private MemberRepository memberRepository;
+import java.util.ArrayList;
+import java.util.List;
 
-    public MemberService(MemberRepository memberRepository){
+@Service
+public class MemberService implements UserDetailsService {
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public MemberService(MemberRepository memberRepository, PasswordEncoder passwordEncoder) {
         this.memberRepository = memberRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        MemberEntity member = memberRepository.findById(username)
+                .orElseThrow(()-> new UserDefineException(username));
+        return new User(member.getId(), member.getPw(), makeGrantedAuthority(member.getRoles()));
+    }
+
+    private List<GrantedAuthority> makeGrantedAuthority(List<MemberRole> roles){
+        List<GrantedAuthority> list = new ArrayList<>();
+        roles.forEach(
+                role -> list.add(
+                        new SimpleGrantedAuthority("ROLE_" + role.getRoleName())
+                )
+        );
+        return list;
     }
 
     public String signUp(MemberDto memberDto){ //회원 가입
@@ -23,9 +53,21 @@ public class MemberService {
             throw new UserDefineException("아이디가 중복됩니다.");
 
         MemberEntity member = memberDto.toEntity();
+        member.setPw(passwordEncoder.encode(member.getPw()));
+
         memberRepository.save(member);
 
         return member.getId();
+    }
+
+    public String signIn(MemberDto memberDto){
+        MemberEntity member = memberRepository.findById(memberDto.getId()).orElseThrow(() -> new UserDefineException("아이디를 잘못 입력하였습니다."));
+
+        if(!passwordEncoder.matches(memberDto.getPw(),member.getPw())){
+            throw new UserDefineException("비밀번호를 잘못 입력하셨습니다.");
+        }
+        //비밀번호 잘못 입력하였을때
+        return null;
     }
 
 
